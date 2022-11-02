7c9e9e7a94710de3f1071cf7ade0d0b8b009099c
VivekSahane <116897401+VivekSahane@users.noreply.github.com>
2022-11-02 19:46:28 +0530

    Add files via upload

---
+++ interview.py
@@ -0,0 +1,24 @@
+#  What are the key features of Python?
+#  What are lists and tuples? What is the key difference between the two?
+#  What are the common built-in data types in Python?
+#  What are modules and packages in Pytho What does immutable mean and what three types of Python core data types are considered immutable?
+#  What are types of Operator are used in Python?
+#  what is the meaning of different operators in python?
+#  What are the comparison operators in Python?
+#  What is pass in Python?
+#  What Does The Continue Do In Python?
+#  When Should You Use The “Break” In Python?  
+#  What Is The Difference Between Pass And Continue In Python?   What gets printed?
+#  What Is A Built-In Function That Python Uses To Iterate Over A Number Sequence?
+#  How does For loop and While loop differ in Python and when do you choose to use them?
+#  Which loop in Python is a counter-controlled loop?
+#  What is the difference between the continue and break statements in Python?
+#  What is the difference between a for loop and a while lo
+#  What are the advantages and disadvantages of using a for loop in Python?
+#  Why Need Python Functions?
+#  What are Python Functions?
+#  Types of Python Functions
+#  Built-in Functions in Python
+#  Python Recursive Function
+#  Python Lambda Function
+#  User-defined Functions in Python
